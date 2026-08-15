5690e1ce9497c5ae5d60ddb770768f00011a16b2
Nassim EL KARATI <nassim.el-karati@etu.unilim.fr>
2026-08-15 13:00:06 +0100

    Fix: types

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ export { BufferWriter } from "./writer.js";
 
 export { BufferReader } from "./reader.js";
 
-export { createBuffer, endianness, type Buffers, type BufferView, type BufferSlice } from "./buffer.js";
+export { createBuffer, endianness, type Buffers, type BufferView } from "./buffer.js";
 
 export { clamp } from "./utils.js";
